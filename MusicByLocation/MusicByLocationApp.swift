94230321be857bdb0ea12071a1b2eb7ae906bd50
William Tao <tao.w@etoncollege.org.uk>
2022-02-26 07:34:29 +0000

    Initial Commit

---
+++ MusicByLocation/MusicByLocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicByLocationApp.swift
+//  MusicByLocation
+//
+//  Created by Tao, Weizhe (Coll) on 26/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct MusicByLocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
